408839a22124a4243479431f146813549e69675d
Cristian19194848 <95430377+Cristian19194848@users.noreply.github.com>
2022-10-22 16:35:13 -0300

    Create verstreamsantiguos.sh
    Sirve para ver streams pasados / antiguos de twitch pero sin sobrecargar la raspberry pi

---
+++ Twitch/verstreamsantiguos.sh
@@ -0,0 +1,15 @@
+clear 
+read -p "ingrese el link de twitch (stream pasado): " video 
+echo "$video" > ./link.txt 
+cut -z -c 28-90 ./link.txt > ./linkdetwitch.txt 
+numero=$(cat ./linkdetwitch.txt) 
+echo "" 
+echo ""
+### echo " link del video es "
+clear 
+echo "" 
+ellink=$(echo "https://player.twitch.tv/?enableExtensions=true&muted=false&parent=twitch.tv&player=popout&video=")
+#cat ./linkdetwitch.txt
+echo "$ellink$numero" 
+sleep 20
+export DISPLAY=:0.0 && xdotool mousemove 226 68 click 1 ; sleep 3; chromium-browser $ellink$numero
